8fb424412de52f658a15e699ae032187b3d80763
Zibilispops <34595511+Zibilispops@users.noreply.github.com>
2026-03-22 09:49:49 +0900

    Update ESLint configuration to turn off rules
    Disable specific ESLint rules for the project.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,16 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
-  ]),
+  ])
+  {
+      rules: {
+              "@typescript-eslint/no-explicit-any": "off",
+                      "react/jsx-no-comment-textnodes": "off",
+                      "@next/next/no-img-element": "off",
+                      "@typescript-eslint/no-unused-vars": "off",
+                      "react/no-unescaped-entities": "off",
+                }
+},
 ]);
 
 export default eslintConfig;
